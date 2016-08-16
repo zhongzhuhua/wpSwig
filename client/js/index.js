0ac8b7d091697a3d80e09088044a5dd99a41bf65
zhongzhuhua <672986035@qq.com>
2016-08-16 09:13:00 +0800

    sass添加

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,7 @@
 console.log('index');
 
+require('scss/index.scss');
+
 var gm = require('gm');
 var $ = require('jquery');
 require('js/hello');
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,21 @@ var router = express.Router();
 // webpack在做自动获取入口路径时，也可以考虑使用此处的slug
 
 module.exports = (app) => {
-	app.use(router.get('/', function(req, res, next) {
-	  res.render('index', { 
-	  	title: '首页',
-	  	slug: 'index'
-	  });
-	}));
+    app.use(router.get('/', function(req, res, next) {
+        res.render('index', {
+            title: '首页',
+            slug: 'index'
+        });
+    }));
+
+    app.use(router.get('/test', function(req, res, next) {
+        res.render('test', {
+            title: '测试',
+            slug: 'test'
+        });
+    }));
+
+    app.use(router.get('/list.html', function(req, res, next) {
+        res.render('list');
+    }));
 }
